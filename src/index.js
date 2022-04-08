9cb314be107ea5a3b74eff59258c228ee9c07b95
kristiannissen <kristian.nissen@gmail.com>
2022-04-08 13:18:48 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ *
+ */
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import {StrictMode} from "react";
+
+const App = () => <div>Hello Kitty</div>;
+
+const rootElm = document.querySelector("#app-root")
+
+console.log(rootElm)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hello Kitty",
+      template: "./src/index.html"
+    })
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./public",
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
+}
