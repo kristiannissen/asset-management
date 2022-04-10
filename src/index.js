4a23e86952aa0fba61ad824d883d7408d68cbab2
Kristian Nissen <kristian.nissen@gmail.com>
2022-04-10 13:10:10 +0200

    Fixes #7

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@
  *
  *
  */
-import React from "react";
+import React, {useEffect} from "react";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 // Tailwind
 import "./styles/shell.css";
 // Custom Components
 import Card from "./components/card";
+// Demo web worker
+const worker = new Worker(new URL("./worker.js", import.meta.url))
 
-const App = () => <div className="pr-4">
+const App = () => {
+    useEffect(() => {
+        worker.postMessage({name: "Pussy"})
+    })
+    return(<div className="pr-4">
         <Card />
-    </div>;
+    </div>);
+}
 
 const rootElm = document.querySelector("#app-root");
 const root = createRoot(rootElm);
